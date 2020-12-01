6464d49f478ad9ef3f52c17a0131a417baf8c401
Aurélien Pupier <apupier@redhat.com>
2020-12-01 14:35:40 +0100

    try using console.log instead of console.error hoping that it will eb visible in the CI trace

--- src/ui-test/extension.test.ts
+++ src/ui-test/extension.test.ts
@@ -114,7 +114,7 @@ export function test(args: TestArguments) {
 		after('Project cleanup', async function () {
 			const projectBackup = path.join('/tmp','wsdl2rest-projectbackup', args.camelVersion, args.type, args.framework);
 			fs.mkdirSync(projectBackup, {recursive: true});
-			console.log('projectBackup'+ projectBackup);
+			console.log('projectBackup: '+ projectBackup);
 			fsExtra.copySync(WORKSPACE_PATH, projectBackup);
 			// remove all files from temp directory
 			for (const f of fs.readdirSync(WORKSPACE_PATH)) {
